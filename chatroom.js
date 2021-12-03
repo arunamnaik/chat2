9a86b4fc15760ae70816ad76034a569f342ba472
arunamnaik <70759516+arunamnaik@users.noreply.github.com>
2021-12-03 12:11:03 +0530

    Add files via upload

---
+++ chatroom.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDsI30qVBrth3Dn-mFJWG4Alx17Ma5OVb4",
+    authDomain: "chat-app-4ebf6.firebaseapp.com",
+    databaseURL: "https://chat-app-4ebf6-default-rtdb.firebaseio.com",
+    projectId: "chat-app-4ebf6",
+    storageBucket: "chat-app-4ebf6.appspot.com",
+    messagingSenderId: "1012287454261",
+    appId: "1:1012287454261:web:06b6623219f784c961c57c",
+    measurementId: "G-R5WSXWHH2Y"
+  };
+  var app = initializeApp(firebaseConfig);
+  var analytics = getAnalytics(app);
